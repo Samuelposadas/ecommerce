0de2c4ff1741286790fc82c4502b881e2442b7af
Samuel Posadas <samugonza99@gmail.com>
2021-12-09 19:47:30 -0300

    resolving bug in the routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { findProductById, getProductsAll } = require("../controllers/products");
 
 router.get("/", getProductsAll);
-router.get("/id:", findProductById);
+router.get("/:id", findProductById);
 
 module.exports = router;
