dbb1870ee8cf703b1ab01b886e929908a09f61d7
Luis <blackkndd@gmail.com>
2021-12-09 11:28:33 -0500

    Creando el modelo Comment

--- api/db/db.js
+++ api/db/db.js
@@ -7,6 +7,7 @@ const modelUser = require("../models/User");
 const modelTypeUser = require("../models/Type_User");
 const modelOrder = require("../models/Order");
 const modelOrderDetail = require("../models/Order_Detail");
+const modelComment = require("../models/Comment");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 
@@ -29,14 +30,14 @@ modelUser(sequelize);
 modelTypeUser(sequelize);
 modelOrder(sequelize);
 modelOrderDetail(sequelize);
-
+modelComment(sequelize);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 
 // eslint-disable-next-line no-unused-vars
 
-const { Product, Category, Supplier, User, Type_User, Order, Order_Detail } = sequelize.models;
+const { Product, Category, Supplier, User, Type_User, Order, Order_Detail, Comment } = sequelize.models;
 
 
 
@@ -44,9 +45,8 @@ const { Product, Category, Supplier, User, Type_User, Order, Order_Detail } = se
 // Product.hasMany(Reviews);
 
 //Creando tabla intermedia de muchos a muchos de productos y categorías de productos
-Product.belongsToMany(Category, {through: "Product_Category", timestamps:false});
-Category.belongsToMany(Product, {through: "Product_Category", timestamps:false});
-
+Product.belongsToMany(Category, {through: "Product_Category", foreignKey: "id_Product" , timestamps:false});
+Category.belongsToMany(Product, {through: "Product_Category", foreignKey: "id_Category", timestamps:false});
 
 Supplier.hasMany(Product); //Un proveedor tiene muchos Productos
 Product.belongsTo(Supplier); //Muchos productos pertenecen a un proveedor
@@ -55,9 +55,23 @@ Type_User.hasMany(User); // Un tipo de usuario puede pertenecer a multiples usua
 User.belongsTo(Type_User); //Múltiples usuarios pueden tener un tipo
 
 User.hasMany(Order); //Un usuario puede tener múltiples ordenes
-Order.hasMany(User); // Multiples ordenes pueden pertenecer a un usuario
+Order.belongsTo(User); // Multiples ordenes pueden pertenecer a un usuario
 
-// Order, Order_Detail
+Order.hasMany(Order_Detail); //Una orden puede tener múltiples detalles
+Order_Detail.belongsTo(Order); //Múltiples detalles pueden pertenecer a una Orden
+
+Product.hasMany(Order_Detail); //Un producto puede estar en múltiples detalles de la orden
+Order_Detail.belongsTo(Product); // Multiples detalles de la orden pueden tener un producto
+
+//Creando la tabla Favorites
+Product.belongsToMany(User, {through: "Favorite", foreignKey: "id_Producto", timestamps:false});
+User.belongsToMany(Product, {through: "Favorite", foreignKey: "id_User", timestamps:false});
+
+Product.hasMany(Comment, { foreignKey: "id_Product"})
+Comment.belongsTo(Product, { foreignKey: "id_Product"})
+
+User.hasMany(Comment, { foreignKey: "id_User"})
+Comment.belongsTo(User, { foreignKey: "id_User"})
 
 
 module.exports = {
