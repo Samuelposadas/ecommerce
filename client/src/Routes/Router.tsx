1cea248bf9b2d53e41293eb76d195d24a6e4918e
Samuel Posadas <samugonza99@gmail.com>
2021-12-09 19:27:25 -0300

    resolving bug searchbar

--- client/src/Routes/Router.tsx
+++ client/src/Routes/Router.tsx
@@ -5,7 +5,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 //components
 import Home from "../Pages/Home/Home";
 import Navbar from "../common/Navbar/Navbar";
-import Probando from "../Pages/Probando";
 
 const Router: FC = () => {
   return (
@@ -13,7 +12,6 @@ const Router: FC = () => {
       <Routes>
         <Route path="/home" element={<Navbar />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/probando" element={<Probando />} />
 
         <Route path="*" element={<Navigate to="/home" />} />
       </Routes>
