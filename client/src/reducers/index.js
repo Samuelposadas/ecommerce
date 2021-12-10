bb9a445b6a505bb0de1578d298bfb9912bcf6dc4
Edwin Ortiz <edarcode@gmail.com>
2021-12-09 22:02:38 -0500

    (client/src/reducers/index.js) initial configuration of the reducer method

---
+++ client/src/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+  product: {},
+  allProdcuts: [],
+  copyAllProdcuts: [],
+  allCategories: [],
+  filterProducts: [],
+};
+
+export const reducer = (store = initialState, action) => {
+  switch (action.type) {
+    default:
+      return store;
+  }
+};
