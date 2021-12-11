73061e804bae60140ad4205640243b19dbc3e58f
Luis <blackkndd@gmail.com>
2021-12-11 11:21:53 -0500

    eliminamos el create category

--- api/controllers/categories.js
+++ api/controllers/categories.js
@@ -1,28 +1,12 @@
 const { Category } = require("../db/db");
 
-const createCategory = async (req, res) => {
-  const { name } = req.body;
-  try {
-    const [newCategory, created] = await Category.findOrCreate({
-      where: { name },
-    });
-    if (created) {
-      res.send("category created");
-    } else {
-      res.send("category already exists");
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-};
-const getCategory = async (req, res, next) => {
+const getCategory = async (req, res) => {
   try {
     const categories = await Category.findAll();
     res.json(categories);
   } catch (error) {
-    next(error);
+    console.log(error);
   }
-};
+}
 
-module.exports = { createCategory, getCategory };
+module.exports = { getCategory };
