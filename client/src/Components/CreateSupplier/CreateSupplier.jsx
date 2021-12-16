c8172578e13703facb7c09d1a6a06c6857ec087e
Edwin Ortiz <edarcode@gmail.com>
2021-12-16 08:40:34 -0500

    feature:(client/src/Components/CreateSupplier/) created component CreateSupplier

---
+++ client/src/Components/CreateSupplier/CreateSupplier.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyledCreateSupplier } from "./styledCreateSupplier";
+
+const CreateSupplier = () => {
+  return (
+    <StyledCreateSupplier>
+      <form action="">
+        <input type="text" />
+        <button>Enviar</button>
+      </form>
+    </StyledCreateSupplier>
+  );
+};
+
+export default CreateSupplier;
---
+++ client/src/Components/CreateSupplier/styledCreateSupplier.js
@@ -0,0 +1,4 @@
+import styled from "styled-components";
+export const StyledCreateSupplier = styled.div`
+  background-color: red;
+`;
