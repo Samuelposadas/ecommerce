2f6ccaef762c5f391b830b0bfc1aafbc2fb76e61
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2021-12-17 13:55:06 -0300

    feat(RouterApp.js): add route to render categories

--- client/src/Routes/RouterApp.js
+++ client/src/Routes/RouterApp.js
@@ -15,6 +15,7 @@ import Home from "../Pages/Home/Home";
 
 import ProductDetail from "../Pages/ProductDetail/ProductDetail";
 import UpdateProduct from "../Pages/UpdateProduct/UpdateProduct";
+import Categories from "../Components/Categories/Categories";
 
 const RouterApp = () => {
   return (
@@ -28,6 +29,7 @@ const RouterApp = () => {
         <Route path="/update/:id" element={<UpdateProduct />} />
         <Route path="/paypal" element={<Paypal />} />
         <Route path="/create/supplier" element={<CreateSupplier />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
     </BrowserRouter>
   );
