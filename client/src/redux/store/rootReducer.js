e133e98067ed14e39bcd0ec76ab4134feba58601
AugustoGirotti <agugirotti@gmail.com>
2021-12-17 19:23:51 -0300

    adding shopping cart functionality

--- client/src/redux/store/rootReducer.js
+++ client/src/redux/store/rootReducer.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
+import { cartReducer } from "../reducers/cartReducer";
 import { reducer } from "../reducers/productsReducer";
-const rootRedurcer = combineReducers({ products: reducer });
+
+const rootRedurcer = combineReducers({ products: reducer, cart: cartReducer });
 
 export default rootRedurcer;
