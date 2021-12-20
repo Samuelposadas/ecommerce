996e66cf876a7e864244af2cbba8e0c056b54330
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2021-12-20 16:46:48 -0300

    feat(client-->Pages-->AdminCategories.jsx & styledAdminCategories.js): add page AdminCategories

---
+++ client/src/Pages/AdminCategories/AdminCategories.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { StyledAdminCategories } from "./styledAdminCategories";
+
+export default function AdminCategories() {
+  return (
+    <StyledAdminCategories>
+      <div className="categoriesWrapper">Admin Categories</div>
+    </StyledAdminCategories>
+  );
+}
---
+++ client/src/Pages/AdminCategories/styledAdminCategories.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledAdminCategories = styled.div`
+  flex: 4;
+  background-color: tomato;
+`;
