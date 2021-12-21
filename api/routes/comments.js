dcb87fbf8949d2c4ef11dc794d326060101878b2
AugustoGirotti <agugirotti@gmail.com>
2021-12-21 19:58:00 -0300

    added route to edit comment

--- api/routes/comments.js
+++ api/routes/comments.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { createComment } = require("../controllers/comments");
+const { createComment, editComment } = require("../controllers/comments");
 
 const router = express.Router();
 router.post("/", createComment);
+router.put("/edit/:id", editComment);
 
 module.exports = router;
