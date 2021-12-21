a2bc24a678af46b4a5d50a88242350cb1ee55f3d
AugustoGirotti <agugirotti@gmail.com>
2021-12-21 03:02:37 -0300

    adding redux persist to keep the cart state

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import rootRedurcer from "./rootReducer";
+import rootReducer from "./rootReducer";
 // import { reducer } from "../reducers/productsReducer";
 import thunk from "redux-thunk";
+import { persistStore } from "redux-persist";
 
 export const store = createStore(
-  rootRedurcer,
+  rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+export const persistor = persistStore(store);
